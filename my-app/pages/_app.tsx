febdaf45cd54587bd16e2f678cd0d877b69ff9c1
mkk4726 <mkk4726@naver.com>
2023-04-19 20:18:38 +0900

    Add global css using emotion

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app'
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import ApolloSetting from '../src/commons/Apollo'
+import { Global } from '@emotion/react'
+import { globalStyles } from '../src/commons/styles'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ApolloSetting>
+      <Global styles={globalStyles} />
       <Component {...pageProps} />
     </ApolloSetting>
   )
---
+++ my-app/src/commons/styles/index.ts
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: "myfont";
+  }
+
+  @font-face {
+    font-family: "myfont";
+    src: url("/fonts/scifibit.ttf");
+  }
+`
